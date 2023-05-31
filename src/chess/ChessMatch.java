d1aca1762fff09e60f5438cb8c14129f3e83eaf9
Lucas Vasques <lucasvc1996@gmail.com>
2023-05-31 18:57:00 -0300

    Improving board visualization

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,29 +34,29 @@ public class ChessMatch {
 	
 	public void initialSetup() {
 		//First row Whites
-		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
-		board.placePiece(new Knight(board, Color.WHITE), new Position(0, 1));
-		board.placePiece(new Bishop(board, Color.WHITE), new Position(0, 2));
-		board.placePiece(new Queen(board, Color.WHITE), new Position(0, 3));
-		board.placePiece(new King(board, Color.WHITE), new Position(0, 4));
-		board.placePiece(new Bishop(board, Color.WHITE), new Position(0, 5));
-		board.placePiece(new Knight(board, Color.WHITE), new Position(0, 6));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 7));
+		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		board.placePiece(new Knight(board, Color.BLACK), new Position(0, 1));
+		board.placePiece(new Bishop(board, Color.BLACK), new Position(0, 2));
+		board.placePiece(new Queen(board, Color.BLACK), new Position(0, 3));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new Bishop(board, Color.BLACK), new Position(0, 5));
+		board.placePiece(new Knight(board, Color.BLACK), new Position(0, 6));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
 		//First row Blacks
-		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 0));
-		board.placePiece(new Knight(board, Color.BLACK), new Position(7, 1));
-		board.placePiece(new Bishop(board, Color.BLACK), new Position(7, 2));
-		board.placePiece(new Queen(board, Color.BLACK), new Position(7, 3));
-		board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
-		board.placePiece(new Bishop(board, Color.BLACK), new Position(7, 5));
-		board.placePiece(new Knight(board, Color.BLACK), new Position(7, 6));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(7, 7));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
+		board.placePiece(new Knight(board, Color.WHITE), new Position(7, 1));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(7, 2));
+		board.placePiece(new Queen(board, Color.WHITE), new Position(7, 3));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(7, 5));
+		board.placePiece(new Knight(board, Color.WHITE), new Position(7, 6));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
 		
 		for (int i=0; i < 8; i++) {
-			board.placePiece(new Pawn(board, Color.WHITE), new Position(1, i));
+			board.placePiece(new Pawn(board, Color.BLACK), new Position(1, i));
 		}
 		for (int i=0; i < 8; i++) {
-			board.placePiece(new Pawn(board, Color.BLACK), new Position(6, i));
+			board.placePiece(new Pawn(board, Color.WHITE), new Position(6, i));
 		}
 		
 		
