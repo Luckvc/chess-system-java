ed43197f8d13429ef997de6eab3fbb0edd7fbb99
Lucas Vasques <lucasvc1996@gmail.com>
2023-05-31 21:46:34 -0300

    Implemeted turn system

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -12,11 +12,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	
+	private int turn;
+	private Color currentPlayer;	
 	private Board board;
 	
 	public ChessMatch() {
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		board = new Board(8, 8);
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -41,6 +53,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 		
 	}
@@ -56,6 +69,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
 		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
@@ -66,6 +82,11 @@ public class ChessMatch {
 		if (!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The chosen piece can't move to target position");
 		}
+	}
+	
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
@@ -92,13 +113,14 @@ public class ChessMatch {
 		board.placePiece(new Knight(board, Color.WHITE), new Position(7, 6));
 		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
 		
+		/*
 		for (int i=0; i < 8; i++) {
 			board.placePiece(new Pawn(board, Color.BLACK), new Position(1, i));
 		}
 		for (int i=0; i < 8; i++) {
 			board.placePiece(new Pawn(board, Color.WHITE), new Position(6, i));
 		}
-		
+		*/
 		
 	}
 	
