038fcbe2f323cde9c00e466efea8bb80c765aa85
Lucas Vasques <lucasvc1996@gmail.com>
2023-06-02 00:57:04 -0300

    Implemented check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -26,13 +26,13 @@ public class ChessPosition {
 	}
 
 
-	//recebo uma chess position(coluna char, linha num), converto para position(linha num, coluna num)
+	//recebo uma chess position(coluna char, linha num), converto para position(linha num, coluna num)	a1 = 07  
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 		}
 	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
